1e3a41c05ab8649d470b3715cbfe0157f13ca9ca
ednzxt <70592950+ednzxt@users.noreply.github.com>
2026-03-03 20:27:07 +0500

    Update validate_and_format_contacts.hpp
    add telegram
    
    Signed-off-by: ednzxt <70592950+ednzxt@users.noreply.github.com>

--- libs/indexer/validate_and_format_contacts.hpp
+++ libs/indexer/validate_and_format_contacts.hpp
@@ -9,6 +9,7 @@ namespace osm
 std::string ValidateAndFormat_website(std::string const & v);
 std::string ValidateAndFormat_facebook(std::string const & v);
 std::string ValidateAndFormat_instagram(std::string const & v);
+std::string ValidateAndFormat_telegram(std::string const & v);
 std::string ValidateAndFormat_twitter(std::string const & v);
 std::string ValidateAndFormat_vk(std::string const & v);
 std::string ValidateAndFormat_contactLine(std::string const & v);
@@ -16,6 +17,7 @@ std::string ValidateAndFormat_contactLine(std::string const & v);
 bool ValidateWebsite(std::string const & site);
 bool ValidateFacebookPage(std::string const & v);
 bool ValidateInstagramPage(std::string const & v);
+bool ValidateTeleramPage(std::string const & v);
 bool ValidateTwitterPage(std::string const & v);
 bool ValidateVkPage(std::string const & v);
 bool ValidateLinePage(std::string const & v);
